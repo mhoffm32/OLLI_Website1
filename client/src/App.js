9e1e6f0f093654ef507774b2f0f3aa2dd7299b5b
Jacob Dreyer <jdreyer4@uwo.ca>
2024-01-20 18:35:57 -0500

    Update styles and navigation functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import NavigationBar from './NavigationBar';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeOption: 'Home'
+    };
+  }
+
+  changePage = (evt) => {
+    this.setState({
+      activeOption: evt.target.innerText
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <NavigationBar />
+        <NavigationBar activeOption={this.state.activeOption} changePage={this.changePage} />
       </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/NavigationBar.js
+++ client/src/NavigationBar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class NavigationBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeOption: props.activeOption
+        };
+
+    }
+
     render() {
+        const { activeOption } = this.props;
         return (
             <div class="topnav">
-                <a class="active" href="#home">Home</a>
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
+                <a className={activeOption === 'Home' ? 'active' : ''} onClick={evt => this.props.changePage(evt)}>Home</a>
+                <a className={activeOption === 'News' ? 'active' : ''} onClick={evt => this.props.changePage(evt)}>News</a>
+                <a className={activeOption === 'Events' ? 'active' : ''} onClick={evt => this.props.changePage(evt)}>Events</a>
+                <a className={activeOption === 'Fundraising' ? 'active' : ''} onClick={evt => this.props.changePage(evt)}>Fundraising</a>
+                <a className={activeOption === 'Contact' ? 'active' : ''} onClick={evt => this.props.changePage(evt)}>Contact</a>
+                <a className={activeOption === 'About' ? 'active' : ''} onClick={evt => this.props.changePage(evt)}>About</a>
             </div>
         );
     }
