584ebcd0a8729a1f4f1a40881fdf799cf07dc701
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2024-01-24 14:12:02 -0500

    Update Home.js

--- client/src/Components/Pages/Home.js
+++ client/src/Components/Pages/Home.js
@@ -5,8 +5,13 @@ class Home extends Component {
         console.log("Displaying Home page");
         return (
             <div>
+                {/*Add Content here */}
                 <h1>Welcome to the Homepage!</h1>
-                {/* Add your content here */}
+                <h2>About Us</h2>
+                <h3>Vision Statement:</h3>
+                <h4>To be a community of inclusion and a circle of friendship that supports and enhances
+                    the lives of our loved one with intellectual disabilities as well as their whole family</h4>
+                
             </div>
         );
     }
