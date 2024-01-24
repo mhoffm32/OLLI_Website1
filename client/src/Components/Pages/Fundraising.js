c6571638bd703ef9991785041f6e1a51fc0760bf
slionett <slionett@uwo.ca>
2024-01-24 15:53:16 -0500

    added images

--- client/src/Components/Pages/Fundraising.js
+++ client/src/Components/Pages/Fundraising.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 
 class Fundraising extends Component {
     render() {
+        const style = {
+            width: '300px',
+            height: '150px'
+        };
         return (
             <div>
                 <h1>Fundraising Page</h1>
                 <p>Support O.L.L.I. by donating to our wonderful community programs!</p>
+                <img src='\images\Sunset-Community-Foundation-Logo-Colour-1024x453.png' style={style}></img>
+                <a href='https://sunsetcommunityfoundation.ca/'>Grand Bend Sunset Foundation</a>
+                <br />
+                <img src='/images/caregivers.jpg'></img>
                 <a href='https://www.canadahelps.org/en/charities/the-ontario-caregiver-organization/'>Ontario Caregiver's Organization</a>
                 <br/>
-                <a href='https://lambtoncatholic.ca/donate'>Order of Alhambra</a>
+                <img src='/images/algarva.jpeg'></img>
+                <a href='https://algarva168.org/'>Order of Alhambra, Algarva 168</a>
                 <br/>
+                <img src='/images/rockglen.jpeg'></img>
                 <a href='https://www.rockglen.com/'>Rock Glen Resorts</a>
 
 
