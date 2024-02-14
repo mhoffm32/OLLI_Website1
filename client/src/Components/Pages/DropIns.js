b42dcf5836deff095adea9a5ad1a59a2634483c7
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2024-02-14 13:00:42 -0500

    fixing drop in functionality

--- client/src/Components/Pages/DropIns.js
+++ client/src/Components/Pages/DropIns.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
@@ -10,7 +9,8 @@ class DropIns extends Component {
         super(props);
         this.state = {
             dropOffTime: new Date(),
-            pickUpTime: new Date()
+            pickUpTime: new Date(),
+            submissionMessage: '' // Add a state property for the submission message
         };
     }
 
@@ -25,50 +25,58 @@ class DropIns extends Component {
     handleSubmit = (event) => { 
         event.preventDefault();
         const { dropOffTime, pickUpTime } = this.state;
-
-        fetch(`http://localhost:3002/api/dropins/`, { 
+    
+        const formattedDropOff = dayjs(dropOffTime).format('YYYY-MM-DD hh:mm A');
+        const formattedPickUp = dayjs(pickUpTime).format('YYYY-MM-DD hh:mm A');
+    
+        fetch(`http://localhost:3002/dropins/`, { 
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({ 
-                dropOffTime: dropOffTime.toISOString(), 
-                pickUpTime: pickUpTime.toISOString(),
-            })
+                dropOff: formattedDropOff, 
+                pickUp: formattedPickUp,
+            })            
         })
         .then(response => {
-
+            if (response.ok) {
+                this.setState({ submissionMessage: `Drop in registration successful! Your drop off date/time is: ${formattedDropOff}. Your pick up date/time is: ${formattedPickUp}. See you then!` });
+            } else {
+                this.setState({ submissionMessage: 'Submission failed. Please try again.' });
+            }
         })
         .catch(error => {
-
+            this.setState({ submissionMessage: 'Submission failed. Please try again.' });
         });
-    };
+    };      
 
     render() {
-        const { dropOffTime, pickUpTime } = this.state;
+        const { dropOffTime, pickUpTime, submissionMessage } = this.state;
 
         return (
             <div className="drop-in">
                 <form onSubmit={this.handleSubmit}>
                     <h1>Drop In Page</h1>
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs} className="date-picker">
                         <DateTimePicker
                             label="Drop Off Time"
                             value={dayjs(dropOffTime)}
                             onChange={this.handleDropOffTimeChange}
                         />
                     </LocalizationProvider>
-
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs} className="date-picker">
                         <DateTimePicker
                             label="Pick Up Time"
                             value={dayjs(pickUpTime)}
                             onChange={this.handlePickUpTimeChange}
                         />
                     </LocalizationProvider>
-
+                    <br></br>
                     <button type="submit" className="button">Submit</button>  
                 </form>
+                {/* Display the submission message */}
+                {submissionMessage && <p>{submissionMessage}</p>}
             </div>
         );
     }
