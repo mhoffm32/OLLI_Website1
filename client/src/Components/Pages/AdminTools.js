f2e75994a1a7f694be47f1655e74c9803306a50c
slionett <slionett@uwo.ca>
2024-02-16 21:19:17 -0500

    bug fixes

--- client/src/Components/Pages/AdminTools.js
+++ client/src/Components/Pages/AdminTools.js
@@ -7,54 +7,14 @@ import ManageUser from "../Pages/manageUser";
 const AdminTools = ({ changePage, user }) => {
   const [page, setPage] = useState("adminHome");
 
-  useEffect(() => {
-    fetchDates();
-  }, []);
-
-  const fetchDates = async () => {
-    try {
-      const response = await fetch(`/getRegistrations`);
-      if (!response.ok) {
-        console.log("Error fetching data");
-        return;
-      }
-      const data = await response.json();
-      setBookedDates(data);
-      
-    } catch (error) {
-      console.log("Error:", error);
-    const getCurrentPage = () => {
-        let pages = {"uploadLetter" : <UploadLetter/>, "manageUser" : <ManageUser/>, "manageSchedule" : <ManageSchedule/>}
-        return pages[page];
-    }
-  };
-  }
-
+ 
+  const getCurrentPage = () => {
+    let pages = {"uploadLetter" : <UploadLetter/>, "manageUser" : <ManageUser/>, "manageSchedule" : <ManageSchedule/>}
+    return pages[page];
+}
 
   return (
-    <div id="admin-options">
-      <h1 id= "admin-title">Admin Tools Home</h1>
-      <div id="admin-page">
-        {page === "adminHome" ? (
-          <>
-            <button id="admin-menu-btn" onClick={() => setPage("uploadLetter")}>
-              Upload Newsletters
-            </button>
-            <br />
-            <button id="admin-sched-btn" onClick={() => setPage("manageSchedule")}>
-              Manage Schedule
-            </button>
-          </>
-        ) : (
-          <>
-            <button id="return-btn" onClick={() => setPage("adminHome")}>
-              Return
-            </button>
-            {page === "uploadLetter" && <UploadLetter />}
-            {page === "manageSchedule" && <ManageSchedule />} 
-          </>
-        )}
-      </div>
+   
     <div id="admin-page">
   
         {page === "adminHome" ? <>
@@ -76,7 +36,7 @@ const AdminTools = ({ changePage, user }) => {
         {getCurrentPage()}
         </>}
     </div>
-    </div>
+
   );
 };
 
