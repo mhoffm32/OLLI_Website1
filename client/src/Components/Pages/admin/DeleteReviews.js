f8542c1125a53096461d2281897bb41dafdec956
Madelyn Hoffman <mhoffm32@uwo.ca>
2024-04-04 20:31:08 -0400

    CSS bug fixes

--- client/src/Components/Pages/admin/DeleteReviews.js
+++ client/src/Components/Pages/admin/DeleteReviews.js
@@ -1,84 +1,87 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-function speak() {  
-    // Create a SpeechSynthesisUtterance object
-  
-    let text = "Here you can delete Reviews"
-  
-    const utterance = new SpeechSynthesisUtterance(text);
-    
-    // Speak the text
-    window.speechSynthesis.speak(utterance);
-  } 
+function speak() {
+  // Create a SpeechSynthesisUtterance object
+
+  let text = "Here you can delete Reviews";
+
+  const utterance = new SpeechSynthesisUtterance(text);
+
+  // Speak the text
+  window.speechSynthesis.speak(utterance);
+}
 
 function DeleteReviews() {
-    const [reviews, setReviews] = useState([]);
-    const [selectedReview, setSelectedReview] = useState(null);
-    const [text, setText] = useState('');
+  const [reviews, setReviews] = useState([]);
+  const [selectedReview, setSelectedReview] = useState(null);
+  const [text, setText] = useState("");
 
-    useEffect(() => {
-        fetch('/getReviews')
-            .then(response => response.json())
-            .then(data => {
-                setReviews(data);
-            })
-            .catch(error => console.error('Error fetching reviews:', error));
-            speak();
-    }, []);
-    
-   const selectReview = (event) => {
-        setSelectedReview(JSON.parse(event.target.value));
+  useEffect(() => {
+    fetch("/getReviews")
+      .then((response) => response.json())
+      .then((data) => {
+        setReviews(data);
+      })
+      .catch((error) => console.error("Error fetching reviews:", error));
+    //speak();
+  }, []);
+
+  const selectReview = (event) => {
+    setSelectedReview(JSON.parse(event.target.value));
+  };
+
+  const deleteReview = async () => {
+    setText("");
+    if (!selectedReview) {
+      setText("Please select a review to delete");
+      return;
     }
+    try {
+      const response = await fetch("/deleteReview", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(selectedReview),
+      });
+      const res = await response.json();
+      if (response.ok) {
+        console.log("Review successfully deleted");
+        setText("Review successfully deleted");
+        fetch("/getReviews")
+          .then((response) => response.json())
+          .then((data) => {
+            setReviews(data);
+          })
+          .catch((error) => console.error("Error fetching reviews:", error));
+        setSelectedReview(null);
+      } else {
+        setText("Error occurred while deleting review");
+        console.log("error occurred: ", res);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const deleteReview = async () => {
-        setText('');
-        if (!selectedReview) {
-            setText('Please select a review to delete');
-            return;
-        }
-        try {
-            const response = await fetch('/deleteReview', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(selectedReview),
-            });
-            const res = await response.json();
-            if (response.ok) {
-                console.log("Review successfully deleted");
-                setText("Review successfully deleted");
-                fetch('/getReviews')
-                    .then(response => response.json())
-                    .then(data => {
-                        setReviews(data);
-                    })
-                    .catch(error => console.error('Error fetching reviews:', error));
-                setSelectedReview(null);
-            } else {
-                setText("Error occurred while deleting review");
-                console.log("error occurred: ", res);
-            }
-        } catch(error) {
-            console.log(error);
-        }
-    };
-
-    return (
-        <div className="deleteReviews">
-            <h1>Delete Reviews</h1>
-            <select onChange={selectReview}>
-                <option value="">Select a review to delete</option>
-                {reviews.map(review => (
-                    <option key={review._id} value={JSON.stringify(review)}>
-                        {review.username} - {review.date} - {review.rating} - {review.content}
-                    </option>
-                ))}
-            </select>
-            <button id='delete-review-btn' onClick={deleteReview}>Delete</button>
-            <p>{text}</p>   
-        </div>
-    );
+  return (
+    <div className="deleteReviews">
+      <h1>Delete Reviews</h1>
+      <select onChange={selectReview}>
+        <option value="">Select a review to delete</option>
+        {reviews.map((review) => (
+          <option key={review._id} value={JSON.stringify(review)}>
+            {review.username} - {review.date} - {review.rating} -{" "}
+            {review.content}
+          </option>
+        ))}
+      </select>
+      <button id="delete-review-btn" onClick={deleteReview}>
+        Delete
+      </button>
+      <p>{text}</p>
+    </div>
+  );
 }
 
 export default DeleteReviews;
--- client/src/Components/Pages/admin/ManageSchedule.js
+++ client/src/Components/Pages/admin/ManageSchedule.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
-import FullCalendar  from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-
-
-
 
 const ManageSchedule = ({ changePage, user }) => {
   const [bookedDates, setBookedDates] = useState([]);
@@ -30,34 +27,45 @@ const ManageSchedule = ({ changePage, user }) => {
     const title = info.event.title;
     const start = info.event.start;
     const end = info.event.end;
-    
+
     // Convert start time to UTC time and format
-    const startTime = start.toLocaleString( { hour: 'numeric', minute: '2-digit' });
+    const startTime = start.toLocaleString({
+      hour: "numeric",
+      minute: "2-digit",
+    });
     let alertMessage = "Event: " + title + " From: " + startTime;
-    
+
     // Check if end time is available
     if (end) {
       // Convert end time to UTC time and format
-      const endTime = end.toLocaleString( {  hour: 'numeric', minute: '2-digit' });
+      const endTime = end.toLocaleString({
+        hour: "numeric",
+        minute: "2-digit",
+      });
       alertMessage += " To: " + endTime;
     } else {
       alertMessage += " (No end time)";
     }
-    
+
     alert(alertMessage);
   };
-  
-  
- 
+
   return (
     <div id="fullcalendar">
       <FullCalendar
         plugins={[dayGridPlugin]}
+        id="calendar-id"
         initialView="dayGridMonth"
         events={bookedDates.map((date) => ({
           title: date.event,
-          start: date.pickup.toLocaleString([], {hour: 'numeric', minute: '2-digit'}),
-          end: date.dropoff.toLocaleString([], {hour: 'numeric', minute: '2-digit'})  
+          start: date.pickup.toLocaleString([], {
+            hour: "numeric",
+            minute: "2-digit",
+          }),
+          end: date.dropoff.toLocaleString([], {
+            hour: "numeric",
+            minute: "2-digit",
+          }),
         }))}
         eventDisplay="list-item"
         eventClick={eventClicked}
