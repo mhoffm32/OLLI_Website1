25ae80b7bd3f369836af797b309242bad01a4d4c
Madelyn Hoffman <mhoffm32@uwo.ca>
2024-05-02 16:22:55 -0400

    ignoring

--- client/src/Components/Pages/GLogin.js
+++ client/src/Components/Pages/GLogin.js
@@ -1,33 +1,28 @@
 import { useState } from "react";
-import {jwtDecode} from "jwt-decode";
-import { GoogleLogin } from '@react-oauth/google';
-import { googleLogout } from '@react-oauth/google';
-import Home from '../Pages/Home'
+import { jwtDecode } from "jwt-decode";
+import { GoogleLogin } from "@react-oauth/google";
+import { googleLogout } from "@react-oauth/google";
+import Home from "../Pages/Home";
 
-const CLIENT_ID =
-  "227573894857-410pmafu7279nappmuv7gfkuf2t32bt0.apps.googleusercontent.com";
-  
+const CLIENT_ID = "placeholder_ID";
+
 const GLogin = ({ changePage }) => {
-
   const [user, setUser] = useState(null);
-  
-  const handleSuccess = (credentials) => {
 
-  
-    console.log('Login Success: ', credentials);
+  const handleSuccess = (credentials) => {
+    console.log("Login Success: ", credentials);
 
     // Send the authorization code to the backend
-    const info = jwtDecode(credentials.credential)
+    const info = jwtDecode(credentials.credential);
     sendAuthorizationCode(credentials.credential);
-   
   };
 
   const sendAuthorizationCode = async (code) => {
     try {
-      const response = await fetch('/google-auth', {
-        method: 'POST',
+      const response = await fetch("/google-auth", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           code: code,
@@ -35,29 +30,28 @@ const GLogin = ({ changePage }) => {
       });
 
       const u = await response.json();
-      localStorage.setItem('jwt', u.token); 
-      const usr = u.token
-      setUser(usr)
-      console.log(jwtDecode(usr))
-  
-      changePage('Home') 
+      localStorage.setItem("jwt", u.token);
+      const usr = u.token;
+      setUser(usr);
+      console.log(jwtDecode(usr));
 
+      changePage("Home");
     } catch (error) {
-      console.error('Error communicating with backend:', error.message);
+      console.error("Error communicating with backend:", error.message);
     }
-};
-  
+  };
+
   const handleFailure = () => {
-    console.log('Login Failed');
+    console.log("Login Failed");
     // You can handle the failed login here, e.g., show an error message.
   };
-  
+
   const handleLogout = () => {
     googleLogout();
-    console.log("logging out")
-    setUser(null)
-    //console.log("isSignedIn: ", isSignedIn, "User: ", user ) 
-  }
+    console.log("logging out");
+    setUser(null);
+    //console.log("isSignedIn: ", isSignedIn, "User: ", user )
+  };
 
   return (
     <div id="g-login-btn">
